22856c8dca90e0aa230773b820c2faab898f1ae7
petercrumb <d4v3.uk@googlemail.com>
2012-09-21 18:49:29 +0100

    Added new examples.

--- src/main.c
+++ src/main.c
@@ -3,14 +3,28 @@
 
 int main(void)
 {
-	split_t *mine;
-	/*  mine = pcre_split("(\\d+)", "hello123world333thisx2is1a616test");
-	    pcre_split_print(mine);
-	    pcre_split_free(mine);*/
+	printf("\nTest 1\n");
+	{
+		split_t *test_1;
+		char *regex = "\\d+";
+		char *string = "hello1world22this333is4444a5555test666666.";
+		/* Pass RegEx and string as char pointers */
+		/* This example splits at multiple 0-9 */
+		test_1 = pcre_split(regex, string);
+		pcre_split_print(test_1);
+		pcre_split_free(test_1);
+	}
 
-	mine = pcre_split("(\\.)", "192.168.0.1");
-	pcre_split_print(mine);
-	pcre_split_free(mine);
-
+	printf("\nTest 2\n");
+	{
+		split_t *test_2;
+		char *regex = "%[\\+#-]?(\\d+|\\*)?\\.?\\d*([hlLzjt]|[hl]{2})?([csuxXfFeEpgGdionz])";
+		char *string = "printf(\"%04x\\n\", variable);";
+		/* Pass RegEx and string as char pointers */
+		/* This example splits at valid printf format variables */
+		test_2 = pcre_split(regex, string);
+		pcre_split_print(test_2);
+		pcre_split_free(test_2);
+	}
 	return 0;
 }
